cc4f9ce6fa5ab3f6cd5114290f50ceb64bab05fe
saptarinoTI <saptarino.ti@gmail.com>
2021-11-12 17:09:18 +0800

    update

--- app/Http/Controllers/JudulTugasAkhirController.php
+++ app/Http/Controllers/JudulTugasAkhirController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Mahasiswa;
 use App\Models\Pendadaran\Pendadaran;
+use App\Models\Proposal1;
 use App\Models\Proposal\Proposal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\Facades\DataTables;
 
 class JudulTugasAkhirController extends Controller
@@ -13,12 +15,12 @@ class JudulTugasAkhirController extends Controller
     public function index(Request $request)
     {
         if ($request->ajax()) {
-            $data = Proposal::where('status', '=', 'diterima')->get();
+            $data = Proposal1::where('status', '=', 'diterima')->get();
             return DataTables::of($data)
                 ->addIndexColumn()
                 ->addColumn('daftar_ta', function ($data) {
-                    return '<p style="margin: 2px 0; font-size: 14px; font-weight: 600">' . ucwords($data->mahasiswa->proposal->judul_ta) . '</p>
-                <p style="margin: 0; font-size: 14px; font-weight: 500; color: #aeaeae">' . ucwords($data->mahasiswa->proposal->mahasiswa->nama) . ' (' . $data->mahasiswa->proposal->mahasiswa->nim . ')</p>';
+                    return '<p style="margin: 2px 0; font-size: 14px; font-weight: 600">' . ucwords($data->judul_ta) . '</p>
+                <p style="margin: 0; font-size: 14px; font-weight: 500; color: #aeaeae">' . ' (' . $data->mahasiswa_nim . ')</p>';
                 })
                 ->rawColumns(['daftar_ta'])
                 ->make(true);
---
+++ app/Models/Proposal1.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Dosen\Dosen;
+use App\Models\Mahasiswa;
+use App\Models\Pendadaran\Pendadaran;
+use App\Models\Seminar\Seminar;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Proposal1 extends Model
+{
+    use HasFactory;
+    protected $table = 'proposal1';
+    protected $fillable = [
+        'mahasiswa_nim', 'file1', 'file2', 'file3', 'tgl_terima', 'utama_id', 'pendamping_id', 'judul_ta', 'status', 'keterangan'
+    ];
+
+    public function mahasiswa()
+    {
+        return $this->belongsTo(Mahasiswa::class);
+    }
+
+    public function dosen1()
+    {
+        return $this->belongsTo(Dosen::class, 'utama_id', 'niy');
+    }
+    public function dosen2()
+    {
+        return $this->belongsTo(Dosen::class, 'pendamping_id', 'niy');
+    }
+
+    public function seminar()
+    {
+        return $this->hasOne(Seminar::class);
+    }
+
+    public function pendadaran()
+    {
+        return $this->hasOne(Pendadaran::class);
+    }
+}
