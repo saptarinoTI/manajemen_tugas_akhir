5e5503b67cd185f5feb3fb43f589d94fdf705f90
saptarinoTI <saptarino.ti@gmail.com>
2021-11-13 16:40:23 +0800

    update

--- resources/views/admin/dosen/index.blade.php
+++ resources/views/admin/dosen/index.blade.php
@@ -1,15 +1,16 @@
 <x-app-layout>
-    {{--
-    <x-app.title name="Seminar Hasil" /> --}}
+    {{-- <x-app.title name="Seminar Hasil" /> --}}
     <x-slot name="name">Data Dosen Pembimbing</x-slot>
 
     <div class="card">
         <div class="card-head mx-4 mt-4 d-flex justify-content-end">
             {{-- <a href="{{ route('dosen.store') }}" class="btn btn-sm text-sm btn-primary">Update Data</a> --}}
-            <form action="{{ route('dosen.store') }}" method="post">
-                @csrf
-                <button type="submit" class="btn btn-sm text-sm btn-primary">Update Data</button>
-            </form>
+            @if (auth()->user()->hasRole('superadmin'))
+                <form action="{{ route('dosen.store') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-sm text-sm btn-primary">Update Data</button>
+                </form>
+            @endif
         </div>
         <div class="card-body">
             <div class="table-responsive">
@@ -22,10 +23,10 @@
                     </thead>
                     <tbody>
                         @foreach ($dosen as $dos)
-                        <tr>
-                            <td>{{ ucwords($dos->niy) }}</td>
-                            <td>{{ ucwords($dos->nama) }}</td>
-                        </tr>
+                            <tr>
+                                <td>{{ ucwords($dos->niy) }}</td>
+                                <td>{{ ucwords($dos->nama) }}</td>
+                            </tr>
                         @endforeach
                     </tbody>
                 </table>
@@ -34,39 +35,39 @@
     </div>
 
     @push('script')
-    <script>
-        // button destroy
-        $('#tabel_role').on('click', '.btn-delete', function(e) {
-            e.preventDefault();
-            var me = $(this),
-                url = me.attr('href'),
-                csrf_token = $('meta[name="csrf-token"]').attr('content');
-            swal({
-                    title: "Hapus Data ?",
-                    icon: "warning",
-                    buttons: true,
-                    dangerMode: true,
-                })
-                .then((willDelete) => {
-                    if (willDelete) {
-                        $.ajax({
-                            type: "POST",
-                            url: url,
-                            data: {
-                                '_method': 'DELETE',
-                                '_token': csrf_token
-                            },
-                            success: function(response) {
-                                location.reload();
-                                swal({
-                                    text: "Data berhasil dihapus!",
-                                    icon: "success",
-                                });
-                            }
-                        });
-                    }
-                });
-        });
-    </script>
+        <script>
+            // button destroy
+            $('#tabel_role').on('click', '.btn-delete', function(e) {
+                e.preventDefault();
+                var me = $(this),
+                    url = me.attr('href'),
+                    csrf_token = $('meta[name="csrf-token"]').attr('content');
+                swal({
+                        title: "Hapus Data ?",
+                        icon: "warning",
+                        buttons: true,
+                        dangerMode: true,
+                    })
+                    .then((willDelete) => {
+                        if (willDelete) {
+                            $.ajax({
+                                type: "POST",
+                                url: url,
+                                data: {
+                                    '_method': 'DELETE',
+                                    '_token': csrf_token
+                                },
+                                success: function(response) {
+                                    location.reload();
+                                    swal({
+                                        text: "Data berhasil dihapus!",
+                                        icon: "success",
+                                    });
+                                }
+                            });
+                        }
+                    });
+            });
+        </script>
     @endpush
 </x-app-layout>
